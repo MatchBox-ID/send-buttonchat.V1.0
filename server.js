73016b2088c21a7c701343ae0f81caf706262a79
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-11-03 14:47:15 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+WAConnection
+WABaileys
+WAProto
+{
+   "[{'idReply'}]:'${allchat}"
+   "[{'BotMsg'}]":'Halo Saya Matchbox-MD, Saya adalah Assistan Bot Resmi WhatsApp',
+                  'footerTeks:${botname}
+                  'fakeReply:${botname}
+                  '[{buttonId, index:1, : buttonType:RESPONSE, buttonText:Ok, type:Ok}]',[{'response':Bot masih beta yah jangan di spam}]
+                  '[{buttonId, index:2, : buttonType:RESPONSE, buttonText:Ok, type:Ok}]',[{'response':Bot masih beta yah jangan di spam}]
+// menu nya belum fix tolong di fix 😁😁😁
+// bisa tambahin fitur sesuka lu
+// Simple whatsapp bot, fitur 1+ wkwkwkw makanya jangan mau yang instant
